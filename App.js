509bd1e026f0eacaa15957b438a16946ce549fbf
alex.villecco <alex.villecco@globant.com>
2018-11-16 13:10:52 -0300

    Without Redux

--- App.js
+++ App.js
@@ -1,31 +1,12 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, } from 'react-native';
+import CounterApp from './src/CounterApp'
 
 export default class App extends Component {
-  state = {
-    counter: 0
-  }
-
-  increaseCounter = () => {
-    this.setState({ counter: this.state.counter + 1 })
-  }
-
-  decreaseCounter = () => {
-    this.setState({ counter: this.state.counter - 1 })
-  }
-
   render() {
     return (
       <View style={styles.container}>
-        <View style={{ flexDirection: 'row', width: 200, justifyContent: 'space-around' }}>
-          <TouchableOpacity onPress={() => this.decreaseCounter()}>
-            <Text style={{ fontSize: 20 }}>Menos</Text>
-          </TouchableOpacity>
-          <Text style={{ fontSize: 20 }}>{this.state.counter}</Text>
-          <TouchableOpacity onPress={() => this.increaseCounter()}>
-            <Text style={{ fontSize: 20 }}>Más</Text>
-          </TouchableOpacity>
-        </View>
+        <CounterApp />
       </View>
     );
   }
---
+++ src/CounterApp.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, } from 'react-native';
+
+export default class CounterApp extends Component {
+  state = {
+    counter: 0
+  }
+
+  increaseCounter = () => {
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+  decreaseCounter = () => {
+    this.setState({ counter: this.state.counter - 1 })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{ flexDirection: 'row', width: 200, justifyContent: 'space-around' }}>
+          <TouchableOpacity onPress={() => this.decreaseCounter()}>
+            <Text style={{ fontSize: 20 }}>Menos</Text>
+          </TouchableOpacity>
+          <Text style={{ fontSize: 20 }}>{this.state.counter}</Text>
+          <TouchableOpacity onPress={() => this.increaseCounter()}>
+            <Text style={{ fontSize: 20 }}>Más</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#eee',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
